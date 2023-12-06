abe99237d096000749c00748298ca44cd80efc26
unknown <adrian.garcia.s.1607@gmail.com>
2023-12-06 01:43:17 -0800

    removed file and made changes to the front end started connecting db to the front end

--- src/Pages/homePage.js
+++ src/Pages/homePage.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+
 export default function HomePage() {
+
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    useEffect(() => {
+        const fetchAvalibility = async () => {
+            const response = await fetch()
+        }
+    }, [])
+
     return (
-        <div className="home">
+        <div className="container form-control">
             <h2>Username</h2>
+            <table className="table table-striped" >
+                <thead>
+                    <tr>
+                        {days.map((day, index) => (
+                        <th scope="col" key={index}>{day}</th>
+                        ))}
+                    </tr>
+                </thead>
+            <tbody>
+
+            </tbody>
+        </table>   
         </div>
     )
 };
